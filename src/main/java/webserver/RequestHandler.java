bf54463cb227033936d65a98a6e0aac1d71977d4
yeonhee <yeonhee7935@naver.com>
2024-03-15 16:38:35 +0900

    :sparkles: [feat] url에 따라 적절한 파일 읽어서 응답 보내기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +21,19 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // 1. Parse Request
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+            if(line==null || line.isEmpty()) throw new RuntimeException("first line should not empty!");
+            String[] tokens = line.split(" ");
+            if (tokens.length != 3) throw new RuntimeException("first line should not empty!");
+            String url = tokens[1];
+            while(!line.isEmpty()){
+                line = br.readLine();
+            }
+            // 2. Send Response
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
