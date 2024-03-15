e080b268b88a7912da6924024ed942b7f6cbfa48
yeonhee <yeonhee7935@naver.com>
2024-03-15 17:06:23 +0900

    :sparkles: [feat] GET user/create 요청에 따른 회원가입 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,16 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,21 +28,32 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // 1. Parse Request
+            // 1. Parse Request Headers
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String line = br.readLine();
-            if(line==null || line.isEmpty()) throw new RuntimeException("first line should not empty!");
+            if (line == null || line.isEmpty()) throw new RuntimeException("first line should not empty!");
             String[] tokens = line.split(" ");
-            if (tokens.length != 3) throw new RuntimeException("first line should not empty!");
+            if (tokens.length != 3) throw new RuntimeException("first line should be like (method url version)!");
             String url = tokens[1];
-            while(!line.isEmpty()){
+            while (!line.isEmpty()) {
                 line = br.readLine();
             }
-            // 2. Send Response
+            // 2.
+            byte[] responseBody = null;
+            if(url.startsWith("/user/create")){
+                int index = url.indexOf("?");
+                String query = url.substring(index+1);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(query);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                DataBase.addUser(user);
+                responseBody = "회원가입이 완료되었습니다.".getBytes();
+            }else{
+                responseBody = Files.readAllBytes(new File("./webapp"+url).toPath());
+            }
+            // 3. Send Response
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            response200Header(dos, responseBody.length);
+            responseBody(dos, responseBody);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
